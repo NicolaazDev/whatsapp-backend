3d7cf27c508375b9dedf5b0204f2ebb26baad840
NicolaazDev <johanhenrique2k24@gmail.com>
2024-12-09 02:21:34 -0300

    ad

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
